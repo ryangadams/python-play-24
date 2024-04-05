ba8077a37605dfc0647d79401928ef69852f8d41
Ryan Adams <ryan.adams@instil.co>
2024-04-05 14:53:02 +0100

    Palindromes incomplete

---
+++ games/palindromes.py
@@ -0,0 +1,21 @@
+data1 = "A man, a plan, a canal, Panama"
+data2 = "Gold is where you find it"
+data3 = "If I had a hi-fi"
+data4 = "Fortune favors the prepared mind"
+data5 = "Rats live on no evil star"
+data6 = "There is no abstract living"
+data7 = "Some men interpret nine memos"
+
+
+def check_for_palindrome(input):
+    pass
+
+
+def play_palindromes():
+    check_for_palindrome(data1)
+    check_for_palindrome(data2)
+    check_for_palindrome(data3)
+    check_for_palindrome(data4)
+    check_for_palindrome(data5)
+    check_for_palindrome(data6)
+    check_for_palindrome(data7)
