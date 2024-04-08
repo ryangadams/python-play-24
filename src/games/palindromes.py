76708e63133382137411096fb8003405c4fddb46
Ryan Adams <ryan.adams@instil.co>
2024-04-08 22:49:21 +0100

    A palindromes solution

--- src/games/palindromes.py
+++ src/games/palindromes.py
@@ -1,21 +1,42 @@
-data1 = "A man, a plan, a canal, Panama"
-data2 = "Gold is where you find it"
-data3 = "If I had a hi-fi"
-data4 = "Fortune favors the prepared mind"
-data5 = "Rats live on no evil star"
-data6 = "There is no abstract living"
-data7 = "Some men interpret nine memos"
+import re
+
+sayings = [
+    "A man, a plan, a canal, Panama",
+    "Gold is where you find it",
+    "If I had a hi-fi",
+    "Fortune favors the prepared mind",
+    "Rats live on no evil star",
+    "There is no abstract living",
+    "Some men interpret nine memos",
+]
 
 
-def check_for_palindrome(input):
-    print(input)
+def sanitise(candidate):
+    return re.sub(r"\W+", "", candidate.lower())
+
+
+def reverse(cleaned):
+    """
+
+    >>> reverse("abcde")
+    'edcba'
+
+    """
+    return cleaned[::-1]
+
+
+def is_palindrome(candidate):
+    cleaned = sanitise(candidate)
+    return cleaned == reverse(cleaned)
+
+
+def check_for_palindrome(phrase):
+    def may_or_may_not_be(candidate):
+        return "IS" if is_palindrome(candidate) else "IS NOT"
+
+    print(f"{phrase} {may_or_may_not_be(phrase)} a palindrome")
 
 
 def play_palindromes():
-    check_for_palindrome(data1)
-    check_for_palindrome(data2)
-    check_for_palindrome(data3)
-    check_for_palindrome(data4)
-    check_for_palindrome(data5)
-    check_for_palindrome(data6)
-    check_for_palindrome(data7)
+    for phrase in sayings:
+        check_for_palindrome(phrase)
